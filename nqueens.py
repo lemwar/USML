1b558eed55751a297c97ebb48382a87878e14804
lemwar <lemwwwar@gmail.com>
2018-01-28 14:44:20 +0500

    Refactor code

--- nqueens.py
+++ nqueens.py
@@ -50,12 +50,12 @@ class Solver_8_queens:
         
         return max_fitness, epochs, self.visualization(self.population[max_index])
     
-    def visualization(self, individual_2):
-        individual = self.convert_to_dec(individual_2)
+    def visualization(self, chromosome_2):
+        chromosome = self.convert_to_dec(chromosome_2)
         s = ''
         for i in range(self.n):
             for j in range(self.n):
-                if individual[i] == j:
+                if chromosome[i] == j:
                     s += 'Q'
                 else:
                     s += '+'
@@ -63,25 +63,25 @@ class Solver_8_queens:
             s += '\n'
         return s
     
-    def generate_individual(self):
+    def generate_chromosome(self):
         s = ''
         for i in range(self.n_bit):
             s += str(random.randint(0, 1))
         return s
             
     # Find a number of attacked pairs of Queens 
-    def fitness(self, individual_2):
-        individual = self.convert_to_dec(individual_2)
+    def fitness(self, chromosome_2):
+        chromosome = self.convert_to_dec(chromosome_2)
         res = 0
         for i in range(self.n):
-            gen = individual[i]
+            gen = chromosome[i]
             j = i + 1
             while j < self.n:
                 # Check lines
-                if individual[j] == gen:
+                if chromosome[j] == gen:
                     res += 1
                 # Check diagonal
-                if (j - i) == math.fabs(individual[j] - individual[i]):
+                if (j - i) == math.fabs(chromosome[j] - chromosome[i]):
                     res += 1
                 j += 1
                     
@@ -109,40 +109,40 @@ class Solver_8_queens:
         pairs = random.sample(range(len(cross_pop)), len(cross_pop))
         for i in range(0, len(cross_pop)-1, 2):
             if random.random() <= self.cross_prob:
-                individual_1 = cross_pop[pairs[i]]
-                individual_2 = cross_pop[pairs[i+1]]
-                k = random.randint(1, len(individual_1) - 1)
-                new_individual_1 = individual_1[0:k] + individual_2[k:self.n_bit]
-                new_population.append(new_individual_1)
+                chromosome_1 = cross_pop[pairs[i]]
+                chromosome_2 = cross_pop[pairs[i+1]]
+                k = random.randint(1, len(chromosome_1) - 1)
+                new_chromosome_1 = chromosome_1[0:k] + chromosome_2[k:self.n_bit]
+                new_population.append(new_chromosome_1)
 
-                new_individual_2 = individual_2[0:k] + individual_1[k:self.n_bit]
-                new_population.append(new_individual_2)
+                new_chromosome_2 = chromosome_2[0:k] + chromosome_1[k:self.n_bit]
+                new_population.append(new_chromosome_2)
 
         return new_population
     
     # Swap 2 horizontal stripes in the desk
     def mutation(self, pop):
         new_pop = list()
-        for individual in pop:
+        for chromosome in pop:
             if random.random() <= self.mut_prob:
                 r = random.randint(0, self.n_bit - 1)
-                gen = individual[r]
+                gen = chromosome[r]
                 new_gen = '0'
                 if gen == '0':
                     new_gen = '1'
-                new_individual = individual[0:r] + new_gen + individual[r + 1:self.n_bit]
-                new_pop.append(new_individual)
+                new_chromosome = chromosome[0:r] + new_gen + chromosome[r + 1:self.n_bit]
+                new_pop.append(new_chromosome)
 
         return new_pop
           
     # Make a new population based on the old and new chromosomes
     def form_result_population(self, new_pop):
-        for individual in self.population:
-            new_pop.append(individual)
+        for chromosome in self.population:
+            new_pop.append(chromosome)
 
         new_fitness_list = list()
-        for individual in new_pop:
-            new_fitness_list.append(self.fitness(individual))
+        for chromosome in new_pop:
+            new_fitness_list.append(self.fitness(chromosome))
 
         for i in range(len(self.population)):
             index = new_fitness_list.index(max(new_fitness_list))
@@ -151,12 +151,12 @@ class Solver_8_queens:
             
     def generate_population(self):
         for i in range(self.pop_size):
-            self.population.append(self.generate_individual())
+            self.population.append(self.generate_chromosome())
             self.fitness_list.append(self.fitness(self.population[i]))
             
     def convert_to_dec(self, s):
-        individual = list()
+        chromosome = list()
         for i in range(0, len(s), self.m):
             gen = int(s[i:i+self.m], 2)
-            individual.append(gen)
-        return individual
+            chromosome.append(gen)
+        return chromosome
